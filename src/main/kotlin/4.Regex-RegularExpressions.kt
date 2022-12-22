d8c6d9ff4c12add0177b3e59e794377dd0f583d8
Ahmet Güleç <ahmtgulec@gmail.com>
2022-12-22 23:58:27 +0300

    ""

--- src/main/kotlin/4.Regex-RegularExpressions.kt
+++ src/main/kotlin/4.Regex-RegularExpressions.kt
@@ -1,3 +1,8 @@
+/**
+ *   Verilen bir dizge içinde, geçerli bir eposta adresini bulun.
+ */
+
+
 fun main() {
     val input = "My email is example@gmail.com and my friend's email is friend@yahoo.com"
     val pattern = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b"
@@ -8,3 +13,16 @@ fun main() {
         println(match.value)
     }
 }
+
+/**
+ *   Bu kod, verilen input dizesinde geçerli olan tüm eposta adreslerini bulur ve ekrana yazdırır.
+ *   Örneğin, bu kod çalıştırıldığında, example@gmail.com ve friend@yahoo.com değerleri ekrana yazdırılır.
+ *
+ *   Regex (Regular Expression) ifadeleri, verilen bir dize içinde belirli bir şablonu aramanıza yardımcı olan araçlardır.
+ *   Bu örnekte, \\b ifadesi dize sınırlarını,
+ *   [A-Za-z0-9._%+-]+ ifadesi eposta adresinin kullanılabilecek tüm karakterleri (harfler, sayılar ve özel karakterler),
+ *   @ ifadesi eposta adresinin ayrılış noktasını,
+ *   [A-Za-z0-9.-]+ ifadesi eposta adresinin domain adının kullanılabilecek tüm karakterlerini ve
+ *   \\.[A-Za-z]{2,} ifadesi de domain adının uzantısını belirtir.
+ *   Bu ifadeler birlikte, verilen dizede geçerli olan tüm eposta adreslerini aramaya yarar.
+ */
