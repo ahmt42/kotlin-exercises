427f6f83a9d9fdf9653e34a86c1f185e46903865
Ahmet Güleç <ahmtgulec@gmail.com>
2023-01-18 22:47:37 +0300

    HackerRank in a String

--- src/main/kotlin/6.HackerRank in a String.kt
+++ src/main/kotlin/6.HackerRank in a String.kt
@@ -1,2 +1,21 @@
 // Verilen stringin içinde “hackerrank” yazılıp yazılmaması kontrolünü yapmamızı ve yazıyorsa “YES” yazmıyorsa “NO” dönmemiz isteniyor.
 
+
+fun main() {
+    println(inString("hereiamstackerrank"))
+}
+private fun inString(s: String): String {
+    val hackerrankString = "hackerrank"
+
+    if (s.length < hackerrankString.length) {
+        return "NO"
+    }
+
+    var index = 0
+    for (element in s) {
+        if (index < hackerrankString.length && element == hackerrankString[index]) {
+            index++
+        }
+    }
+    return if (index == hackerrankString.length) "YES" else "NO"
+}
