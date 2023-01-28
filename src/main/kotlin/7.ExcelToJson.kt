3906aa53d39b29f4a203d3b13b0cdc0f85a12918
Ahmet Güleç <ahmtgulec@gmail.com>
2023-01-28 23:57:08 +0300

    Excel to JSON

--- src/main/kotlin/7.ExcelToJson.kt
+++ src/main/kotlin/7.ExcelToJson.kt
@@ -7,8 +7,8 @@ import java.io.FileInputStream
 
 fun main() {
     //val excelFile = File("path/to/excel/file.xlsx")
-    val excelFile = File("./files/Kitap.xlsx")
-    val jsonFile = File("./files/Kitap.json")
+    val excelFile = File("./files/hasta.xlsx")
+    val jsonFile = File("./files/hasta.json")
     excelToJson(excelFile, jsonFile)
 }
 
@@ -17,23 +17,30 @@ fun excelToJson(excelFile: File, jsonFile: File) {
     val sheet = workbook.getSheetAt(0)
     val rows = sheet.iterator()
     val data = mutableListOf<Map<String, Any>>()
+
     while (rows.hasNext()) {
         val row = rows.next()
         val cells = row.cellIterator()
         val obj = mutableMapOf<String, Any>()
+
         while (cells.hasNext()) {
             val cell = cells.next()
             val columnIndex = cell.columnIndex
             val columnName = sheet.getRow(0).getCell(columnIndex).stringCellValue
+
             obj[columnName] = when (cell.cellType) {
                 CellType.STRING -> cell.stringCellValue
                 CellType.NUMERIC -> if (DateUtil.isCellDateFormatted(cell)) cell.dateCellValue else cell.numericCellValue
                 CellType.BOOLEAN -> cell.booleanCellValue
                 else -> ""
             }
+
+            println(obj[columnName])
         }
+
         data.add(obj)
     }
+
     val json = ObjectMapper().writeValueAsString(data)
     jsonFile.writeText(json)
 }
